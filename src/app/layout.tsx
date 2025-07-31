6a0a003b7d7cd1188f4b38af1d2bfa56df2121c4
ahmad sidik <ahmadsidik.27@gmail.com>
2025-07-31 08:51:28 +0000

    warna background ganti dengan warna gelap metalik

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" className={`${inter.variable} h-full`}>
+    <html lang="id" className={`${inter.variable} h-full dark`}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
